7b241c2f9b4a0620bce3cab8486955b36327f10e
Álvaro Castro Castilla <alvcastro@yahoo.es>
2016-04-26 14:08:14 +0200

    Automated grabbing

---
+++ grab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# The tricky way to get find's output into an array
+unset files i
+while IFS= read -r -d $'\0' file; do
+  files[i++]="$file"
+  REF_FILE="${file//$DIR\/gentoo\/}"
+  cp "/$REF_FILE" "$DIR/gentoo/$REF_FILE"
+done < <(find $DIR/gentoo -type f -print0)
