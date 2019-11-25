91ac60b20e700ee975b089891d2ee0d21566df63
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2019-11-25 08:34:35 +0200

    Update config-git.sh

--- base/config-git.sh
+++ base/config-git.sh
@@ -2,3 +2,4 @@
 
 git config --global user.name "Álvaro Castro Castilla" 
 git config --global user.email a@fourthbit.com
+git config --global --add url."git@github.com:".insteadOf "https://github.com/"
